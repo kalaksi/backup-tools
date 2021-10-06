e5c7aed5e61dd5199ff19ac4c7fb8b10d952e64d
kalaksi <kalaksi@users.noreply.github.com>
2021-10-06 19:33:51 +0300

    checksumfile-create: refactor color coding

--- checksumfile-create.sh
+++ checksumfile-create.sh
@@ -29,6 +29,11 @@ declare -i UPDATE_EXISTING=0
 # Parameters for filtering find in find-command. For example, to only include files that are over 50kiB use: -f '-size +50k'
 declare FIND_PARAMS=""
 
+declare C_WHITE=$(printf "\033[1m")
+declare C_GREEN=$(printf "\033[1;32m")
+declare C_RED=$(printf "\033[1;31m")
+declare C_END=$(printf "\033[0m")
+
 function _help {
     cat << EOF
 
@@ -36,7 +41,7 @@ Usage: $0 [-b hash_binary_name] [-d subdir_depth] [-f "find_params"] [-n checksu
 
 Recursively creates checksum files using $HASH_BINARY. Option -d will define the subdirectory level
 on which the files are created. The checksum file will contain checksums of files in and under that directory.
-Integrity verification can be done directly with sha256sum or by using $(echo -e "\033[1mchecksumfile-verify.sh\033[0m").
+Integrity verification can be done directly with sha256sum or by using $(echo -e "${C_WHITE}checksumfile-verify.sh${C_END}").
 
 Options:
   -f  Parameters for 'find' for file filtering. This dictates which files will be included in the checksum generation.
@@ -75,7 +80,7 @@ function checksumfile_update {
     # Add new files that are not hashed already.
     for efile in "${!eligible_files[@]}"; do
         if [ -z "${checksumfile_files[$efile]:-}" ]; then
-            "$hash_binary" "$efile" | tee -a "$checksum_file" | sed -E "s/^[^ ]+/$(printf "    \033[1;32mAdded\033[0m")/" || return 1
+            "$hash_binary" "$efile" | tee -a "$checksum_file" | sed -E "s/^[^ ]+/$(printf "    ${C_GREEN}Added${C_END}")/" || return 1
             changed=1
         fi
     done
@@ -85,7 +90,7 @@ function checksumfile_update {
         if [ -z "${eligible_files[$cfile]:-}" ]; then 
             declare cfile_escaped=$(printf '%s\n' "$cfile" | sed 's/[[\.*^$/]/\\&/g')
             sed -i'' "/^${checksumfile_files[$cfile]}..${cfile_escaped}/d" "$checksum_file" || return 1
-            echo -ne "    \033[1;31mDeleted\033[0m " ; echo "$cfile"
+            echo -ne "    ${C_RED}Deleted${C_END} " ; echo "$cfile"
             changed=1
         fi
     done
@@ -106,20 +111,20 @@ function checksumfile_create {
     declare -i errors=0
 
     readarray -d '' checksum_subdirs < <(find "$main_dir" -maxdepth "$dir_depth" -mindepth "$dir_depth" -type d -print0)
-    echo -e "Processing directory \033[1m$main_dir\033[0m with \033[1m${#checksum_subdirs[@]}\033[0m subdirectories:"
+    echo -e "Processing directory ${C_WHITE}$main_dir${C_END} with ${C_WHITE}${#checksum_subdirs[@]}${C_END} subdirectories:"
     for workdir in "${checksum_subdirs[@]}"; do
         # Use subshell so we won't change the current working directory or environment
         (
-          echo -e "  \033[1m$workdir\033[0m:"
+          echo -e "  ${C_WHITE}$workdir${C_END}:"
           cd -- "$workdir" || exit 1
 
           if [ ! -s "$checksum_file" ]; then
               find_wrapper "$checksum_file" "$find_params" | xargs -r -0 -n1 "$hash_binary" | tee "$checksum_file" | sed -E 's/^[^ ]+/  /' || exit 1
           else
-              echo -ne "    \033[1;33m$(grep -c "^[^#]" "$checksum_file")\033[0m existing checksums available. "
+              echo -ne "    \033[1;33m$(grep -c "^[^#]" "$checksum_file")${C_END} existing checksums available. "
               if [ $update_existing -eq 1 ]; then
                   echo "Checking for new or deleted files... "
-                  checksumfile_update "$hash_binary" "$checksum_file" "$find_params" || { echo -e "    \033[1;31mAn error occurred. Updating aborted.\033[0m" >&2; exit 1; }
+                  checksumfile_update "$hash_binary" "$checksum_file" "$find_params" || { echo -e "    ${C_RED}An error occurred. Updating aborted.${C_END}" >&2; exit 1; }
               else
                   echo "Skipping."
               fi
@@ -129,9 +134,9 @@ function checksumfile_create {
     done
 
     if [ $errors -eq 0 ]; then
-        echo -e "\n\033[1mCompleted without errors.\033[0m"
+        echo -e "\n${C_WHITE}Completed without errors.${C_END}"
     else
-        echo -e "\nEncountered errors with \033[1;31m$errors\033[0m checksum files!" >&2
+        echo -e "\nEncountered errors with ${C_RED}$errors${C_END} checksum files!" >&2
     fi
     return $errors
 }
